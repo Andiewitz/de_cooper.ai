a5a3343131bca3922e150f856076f26aee1b5146
AndreiG. <gracefulandrei@gmail.com>
2026-06-01 17:34:05 +0800

    Cleanup: remove temporary test_import.js script

--- scratch/test_import.js
+++
@@ -1,3 +0,0 @@
-import nextVitals from 'eslint-config-next/core-web-vitals';
-console.log('Successfully imported next/core-web-vitals:', typeof nextVitals);
-console.log('Configuration keys:', Object.keys(nextVitals));
